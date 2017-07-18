b60e203b2d54c598ad3500f9dd6881133140e7dc
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-18 15:35:26 -0600

    update seeding, started basic gen

--- lib/msrta_gen/util.ex
+++ lib/msrta_gen/util.ex
@@ -1,2 +1,26 @@
 defmodule Msrta.Util do
+  import Msrta.Definitions
+
+  def gen_seed(length \\ 16) do
+    characters = for _x <- 1..length do
+      Enum.random(0..9)
+    end
+
+    characters |> Integer.undigits
+  end
+
+  def gen_plateau(seed) do
+    :rand.seed(:exs1024, {seed, 0, 0})
+    Definitions.plateau |> Enum.shuffle
+  end
+
+  def gen_run(params) do
+    total_orbs = 36
+    :rand.seed(:exs1024, {params.seed, 0, 0})
+
+    plateau = case params.plat_shuffle do
+      true -> gen_plateau(params.seed)
+      false -> nil
+    end
+  end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule MsrtaGen.PageController do
   use MsrtaGen.Web, :controller
+  import Msrta.Util
 
   def index(conn, _params) do
-    render conn, "index.html", [seed: Enum.random(1..9999999999999999)]
+    render conn, "index.html", [seed: Util.gen_seed]
   end
 end
