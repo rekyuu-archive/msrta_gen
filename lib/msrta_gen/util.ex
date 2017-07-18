3073bdd2ba20ff09be9f66f03bc1bf56c365f5c5
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-18 15:39:18 -0600

    ????

--- lib/msrta_gen/util.ex
+++ lib/msrta_gen/util.ex
@@ -1,5 +1,5 @@
 defmodule MsrtaGen.Util do
-  import MsrtaGen.Definitions
+  alias MsrtaGen.Definitions
 
   def gen_seed(length \\ 16) do
     characters = for _x <- 1..length do
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,6 @@
 defmodule MsrtaGen.PageController do
   use MsrtaGen.Web, :controller
-  import MsrtaGen.Util
+  alias MsrtaGen.Util
 
   def index(conn, _params) do
     render conn, "index.html", [seed: Util.gen_seed]
